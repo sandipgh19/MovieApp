091ab7914262f377e6ec91a343e36c1dc640051a
sandipghosh <sandipgh19@gmail.com>
2019-03-31 14:51:34 +0530

    fix date isse, layout, finalize design

--- app/src/main/java/sandip/example/com/databinding/binding/FragmentBindingAdapters.kt
+++ app/src/main/java/sandip/example/com/databinding/binding/FragmentBindingAdapters.kt
@@ -2,13 +2,17 @@ package sandip.example.com.databinding.binding
 
 import android.databinding.BindingAdapter
 import android.support.v4.app.Fragment
+import android.support.v4.widget.CircularProgressDrawable
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import sandip.example.com.databinding.objects.MovieGenres
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
 
 /**
@@ -23,7 +27,13 @@ class FragmentBindingAdapters @Inject constructor(val fragment: Fragment)  {
 
     @BindingAdapter("imageUrl")
     fun bindImage(imageView: ImageView, url: String?) {
-        Glide.with(fragment).load(url).into(imageView)
+        val originalUrl = "https://image.tmdb.org/t/p/w500$url"
+        val circularProgressDrawable = CircularProgressDrawable(fragment.requireContext())
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+        Log.e("ImageUrl", "Url $originalUrl")
+        Glide.with(fragment).load(originalUrl).placeholder(circularProgressDrawable).into(imageView)
     }
 
     @BindingAdapter("upperString")
@@ -35,15 +45,42 @@ class FragmentBindingAdapters @Inject constructor(val fragment: Fragment)  {
     fun bindDateTime(textView: TextView, timeStamp: String?, format: String, emptyTxt: String) {
         if (timeStamp != null) {
             val format1 = SimpleDateFormat(
-                "YYYY-MM-DD", Locale.US
+                "yyyy-MM-DD", Locale.US
             )
             format1.timeZone = TimeZone.getTimeZone("UTC")
             val mDate = format1.parse(timeStamp)
-            val dateFormat = SimpleDateFormat(format, Locale.ENGLISH)
+            val calendar = Calendar.getInstance()
+            calendar.time = mDate
+            val dayNumberSuffix = getDayNumberSuffix(calendar.get(Calendar.DAY_OF_MONTH))
+
+            val dateFormat = SimpleDateFormat(format.replace("dd","dd'${dayNumberSuffix}'"), Locale.ENGLISH)
             textView.text = dateFormat.format(mDate)
         } else
             textView.text = emptyTxt
 
+    }
+
+    private fun getDayNumberSuffix(day: Int): String {
+        if (day in 11..13) {
+            return "th"
+        }
+        when (day % 10) {
+            1 -> return "st"
+            2 -> return "nd"
+            3 -> return "rd"
+            else -> return "th"
+        }
+    }
+
+    @BindingAdapter(value = ["genresList"], requireAll = true)
+    fun bindGenres(textView: TextView, list: MutableList<MovieGenres?>?){
+        list?.let {
+            val localList: MutableList<String?> = ArrayList()
+            it.filterIndexed { index, movieGenres ->
+                localList.add(movieGenres?.name)
+            }
+            textView.text = localList.toString().replace("[","").replace("]","")
+        }
     }
 
 }
